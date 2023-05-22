13068c5ae56e0f4f30d85aa892b4527a29314b60
Yunusov <abubakar.yunusov.47@mail.ru>
2023-05-22 16:20:12 +0300

    3task-1day-3week

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import os from 'os'
 console.log(os.hostname())
 console.log(os.homedir())
 console.log(os.version())
-console.log(os.type())
-console.log(`Привет юзер, кажется ты зашёл с ${os.type()}`)
+import { helloUser } from './welcome.js'
+helloUser()
---
+++ welcome.js
@@ -0,0 +1,3 @@
+export const helloUser = () => console.log(`Привет юзер, кажется ты зашёл с ${os.type()}`)
+helloUser()
+import os from 'os'
