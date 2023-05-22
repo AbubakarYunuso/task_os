2ad8b36ad9e6ff113fb156e2ede4a01607f4c474
Yunusov <abubakar.yunusov.47@mail.ru>
2023-05-22 16:10:10 +0300

    3task-1day-3week

---
+++ index.js
@@ -0,0 +1,7 @@
+
+import os from 'os'
+console.log(os.hostname())
+console.log(os.homedir())
+console.log(os.version())
+console.log(os.type())
+console.log(`Привет юзер, кажется ты зашёл с ${os.type()}`)
